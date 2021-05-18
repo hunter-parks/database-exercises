d0d26e5ee6e07cd3166bd3ccf02216da985684cd
Hunter Parks <parks.hunter1992@gmail.com>
2021-05-18 14:16:43 -0500

    added a comment

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,16 +1,17 @@
 
 USE codeup_test_db;
 
-
+-- creating a table called albums
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name VARCHAR(50) DEFAULT 'NONE',
     record_name VARCHAR(50) DEFAULT 'NONE',
-    release_date DATE ,
-    record_sales FLOAT,
+    release_date DATE DEFAULT 'NONE',
+    record_sales FLOAT DEFAULT 'NONE',
     genre VARCHAR(50) DEFAULT 'NONE',
     PRIMARY KEY (id)
 );
+
 
 SELECT * FROM albums;
 DESCRIBE albums;
